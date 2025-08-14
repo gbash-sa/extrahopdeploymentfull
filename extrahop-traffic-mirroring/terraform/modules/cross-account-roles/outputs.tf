8e2d0d97b45974fbce30627154bd2eb8c0b2db1b
gbash-sa <garrett.bashore@rgare.com>
2025-08-14 14:48:38 -0700

    Delete extrahop-traffic-mirroring directory

--- extrahop-traffic-mirroring/terraform/modules/cross-account-roles/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ndr_monitoring_role_arn" {
-  description = "ARN of the NDR monitoring role"
-  value       = aws_iam_role.ndr_monitoring.arn
-}
-
-output "ndr_monitoring_role_name" {
-  description = "Name of the NDR monitoring role"
-  value       = aws_iam_role.ndr_monitoring.name
-}
